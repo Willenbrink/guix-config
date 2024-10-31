ec37bf121e24e8435ffbd6a7a85d0d86db9e3b7f
Sebastian Willenbrink <sebastian.willenbrink@gmail.com>
2024-10-31 22:00:00 +0100

    Update packages

--- my-packages/game-client.scm
+++ my-packages/game-client.scm
@@ -77,152 +77,152 @@
 
 (define hicolor-icon-theme-lutris
   (package
-   (name "hicolor-icon-theme-lutris")
-   (version "0.17-ee8fe23c")
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/lutris/lutris-runtime")
-           (commit "ee8fe23c5bf2ac979efb9c00ca3abbd57af180a8")))
-     (file-name (git-file-name name version))
-     (sha256
-      (base32 "0wjjj46wmnqc8p1hy0q74bpfqf0cm06hd518j170mrm9pnpiil2b"))))
-   (build-system copy-build-system)
-   (arguments
-    (list
-     #:install-plan ''(("icons" "share/icons"))))
-   (home-page "https://github.com/lutris/lutris-runtime")
-   (synopsis "Freedesktop icon theme - Lutris edition")
-   (description "Freedesktop icon theme - Lutris edition.")
-   (license license:gpl2)))
+    (name "hicolor-icon-theme-lutris")
+    (version "0.17-ee8fe23c")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/lutris/lutris-runtime")
+             (commit "ee8fe23c5bf2ac979efb9c00ca3abbd57af180a8")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0wjjj46wmnqc8p1hy0q74bpfqf0cm06hd518j170mrm9pnpiil2b"))))
+    (build-system copy-build-system)
+    (arguments
+     (list
+      #:install-plan ''(("icons" "share/icons"))))
+    (home-page "https://github.com/lutris/lutris-runtime")
+    (synopsis "Freedesktop icon theme - Lutris edition")
+    (description "Freedesktop icon theme - Lutris edition.")
+    (license license:gpl2)))
 
 (define-public lutris
   (package
-   (name "lutris")
-   (version "0.5.17")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (string-append
-           "https://github.com/lutris/lutris/archive/refs/tags/v" version
-           ".tar.gz"))
-     (sha256
-      (base32 "1a0s4s2wf2nmdy7c2axws5q1xg3jnvq6h8ycky1jk36b1g3x04z9"))))
-   (build-system copy-build-system)
-   (propagated-inputs (list ;Otherwise, TLS doesn't work and it's impossible to connect to itch.io, gog, etc.
-                       cairo gsettings-desktop-schemas nss-certs webkitgtk-for-gtk3))
-   (inputs (list
-            ;; Non-python dependencies:
-            file
-            gdk-pixbuf
-            gnutls
-            hicolor-icon-theme-lutris
-            hicolor-icon-theme
-            libpng
-            librsvg
-            mesa
-            shared-mime-info
-            vulkan-loader
-            ;; Will be in the path:
-            fluidsynth
-            glibc
-            `(,gtk+ "bin")
-            mesa-utils
-            p7zip
-            pciutils
-            procps
-            psmisc
-            vulkan-tools
-            xrandr
-            ;; Python dependencies:
-            python
-            python-dbus
-            python-distro
-            python-evdev
-            python-lxml
-            python-pillow
-            python-protobuf
-            python-pycairo
-            python-pygobject
-            python-pyyaml
-            python-requests
-            python-setproctitle))
-   (arguments
-    (list
-     #:phases #~(modify-phases %standard-phases
-                               (add-before 'install 'gdk-pixbuf-cache-gen
-                                           (lambda* (#:key #:allow-other-keys)
-                                             (setenv "GDK_PIXBUF_MODULE_FILE"
-                                                     (string-append #$output
-                                                                    "/lib/gdk-pixbuf/loaders.cache"))
-                                             (mkdir-p (string-append #$output "/lib/gdk-pixbuf"))
-                                             (invoke "gdk-pixbuf-query-loaders" "--update-cache"
-                                                     #$(file-append librsvg
-                                                                    "/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg"))))
-                               (add-before 'install 'ldconfig-p
-                                           (lambda* (#:key #:allow-other-keys)
-                                             (mkdir-p (string-append #$output "/etc"))
-                                             (invoke "ldconfig"
-                                                     "-C"
-                                                     (string-append #$output "/etc/ld.so.cache")
-                                                     #$(file-append gnutls "/lib")
-                                                     #$(file-append mesa "/lib")
-                                                     #$(file-append vulkan-loader "/lib"))
-                                             (substitute* "lutris/util/linux.py"
-                                                          (("\"-p\"")
-                                                           (string-append "\"-C\", \""
-                                                                          #$output "/etc/ld.so.cache\", \"-p\"")))))
-                               (add-before 'install 'patch-hardcoded-paths-and-wine
-                                           (lambda* (#:key #:allow-other-keys)
-                                             (substitute* "lutris/util/linux.py"
-                                                          (("\"wine\",")
-                                                           ""))
-                                             (substitute* "lutris/util/graphics/vkquery.py"
-                                                          (("libvulkan.so.1")
-                                                           #$(file-append vulkan-loader "/lib/libvulkan.so.1")))
-                                             (substitute* "lutris/util/magic.py"
-                                                          (("libmagic.so.1")
-                                                           #$(file-append file "/lib/libmagic.so.1")))
-                                             (substitute* "lutris/util/wine/wine.py"
-                                                          (("os\\.listdir\\('/usr/lib/'\\)")
-                                                           "[]"))))
-                               (add-after 'install 'wrap
-                                          (lambda* (#:key #:allow-other-keys)
-                                            (wrap-program (string-append #$output "/bin/lutris")
-                                                          `("GDK_PIXBUF_MODULE_FILE" =
-                                                            (,(getenv "GDK_PIXBUF_MODULE_FILE")))
-                                                          `("GI_TYPELIB_PATH" =
-                                                            (,(getenv "GI_TYPELIB_PATH")))
-                                                          `("PATH" =
-                                                            ,(list #$(file-append fluidsynth "/bin")
-                                                                   #$(file-append glibc "/sbin")
-                                                                   (string-append #$gtk+:bin "/bin")
-                                                                   #$(file-append mesa-utils "/bin")
-                                                                   #$(file-append p7zip "/bin")
-                                                                   #$(file-append pciutils "/bin")
-                                                                   #$(file-append procps "/bin")
-                                                                   #$(file-append psmisc "/bin")
-                                                                   #$(file-append vulkan-tools "/bin")
-                                                                   #$(file-append xrandr "/bin")
-                                                                   #$(file-append xterm "/bin")
-                                                                   "$PATH"))
-                                                          `("PYTHONPATH" =
-                                                            (,(getenv "GUIX_PYTHONPATH")))
-                                                          `("XDG_DATA_DIRS" =
-                                                            ,(list #$(file-append hicolor-icon-theme-lutris
-                                                                                  "/share")
-                                                                   #$(file-append hicolor-icon-theme "/share")
-                                                                   #$(file-append shared-mime-info "/share")
-                                                                   "$XDG_DATA_DIRS"))))))
-     #:install-plan ''(("bin" "bin")
-                       ("lutris" "lutris")
-                       ("share" "share"))))
-   (home-page "https://github.com/lutris/lutris")
-   (synopsis "Game library manager")
-   (description
-    "Lutris is a game manager that can be used as a frontend for many sources of games")
-   (license license:gpl3)))
+    (name "lutris")
+    (version "0.5.17")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://github.com/lutris/lutris/archive/refs/tags/v" version
+             ".tar.gz"))
+       (sha256
+        (base32 "1a0s4s2wf2nmdy7c2axws5q1xg3jnvq6h8ycky1jk36b1g3x04z9"))))
+    (build-system copy-build-system)
+    (propagated-inputs (list ;Otherwise, TLS doesn't work and it's impossible to connect to itch.io, gog, etc.
+                        cairo gsettings-desktop-schemas nss-certs webkitgtk-for-gtk3))
+    (inputs (list
+             ;; Non-python dependencies:
+             file
+             gdk-pixbuf
+             gnutls
+             hicolor-icon-theme-lutris
+             hicolor-icon-theme
+             libpng
+             librsvg
+             mesa
+             shared-mime-info
+             vulkan-loader
+             ;; Will be in the path:
+             fluidsynth
+             glibc
+             `(,gtk+ "bin")
+             mesa-utils
+             p7zip
+             pciutils
+             procps
+             psmisc
+             vulkan-tools
+             xrandr
+             ;; Python dependencies:
+             python
+             python-dbus
+             python-distro
+             python-evdev
+             python-lxml
+             python-pillow
+             python-protobuf
+             python-pycairo
+             python-pygobject
+             python-pyyaml
+             python-requests
+             python-setproctitle))
+    (arguments
+     (list
+      #:phases #~(modify-phases %standard-phases
+                   (add-before 'install 'gdk-pixbuf-cache-gen
+                     (lambda* (#:key #:allow-other-keys)
+                       (setenv "GDK_PIXBUF_MODULE_FILE"
+                               (string-append #$output
+                                              "/lib/gdk-pixbuf/loaders.cache"))
+                       (mkdir-p (string-append #$output "/lib/gdk-pixbuf"))
+                       (invoke "gdk-pixbuf-query-loaders" "--update-cache"
+                               #$(file-append librsvg
+                                              "/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg"))))
+                   (add-before 'install 'ldconfig-p
+                     (lambda* (#:key #:allow-other-keys)
+                       (mkdir-p (string-append #$output "/etc"))
+                       (invoke "ldconfig"
+                               "-C"
+                               (string-append #$output "/etc/ld.so.cache")
+                               #$(file-append gnutls "/lib")
+                               #$(file-append mesa "/lib")
+                               #$(file-append vulkan-loader "/lib"))
+                       (substitute* "lutris/util/linux.py"
+                         (("\"-p\"")
+                          (string-append "\"-C\", \""
+                                         #$output "/etc/ld.so.cache\", \"-p\"")))))
+                   (add-before 'install 'patch-hardcoded-paths-and-wine
+                     (lambda* (#:key #:allow-other-keys)
+                       (substitute* "lutris/util/linux.py"
+                         (("\"wine\",")
+                          ""))
+                       (substitute* "lutris/util/graphics/vkquery.py"
+                         (("libvulkan.so.1")
+                          #$(file-append vulkan-loader "/lib/libvulkan.so.1")))
+                       (substitute* "lutris/util/magic.py"
+                         (("libmagic.so.1")
+                          #$(file-append file "/lib/libmagic.so.1")))
+                       (substitute* "lutris/util/wine/wine.py"
+                         (("os\\.listdir\\('/usr/lib/'\\)")
+                          "[]"))))
+                   (add-after 'install 'wrap
+                     (lambda* (#:key #:allow-other-keys)
+                       (wrap-program (string-append #$output "/bin/lutris")
+                         `("GDK_PIXBUF_MODULE_FILE" =
+                           (,(getenv "GDK_PIXBUF_MODULE_FILE")))
+                         `("GI_TYPELIB_PATH" =
+                           (,(getenv "GI_TYPELIB_PATH")))
+                         `("PATH" =
+                           ,(list #$(file-append fluidsynth "/bin")
+                                  #$(file-append glibc "/sbin")
+                                  (string-append #$gtk+:bin "/bin")
+                                  #$(file-append mesa-utils "/bin")
+                                  #$(file-append p7zip "/bin")
+                                  #$(file-append pciutils "/bin")
+                                  #$(file-append procps "/bin")
+                                  #$(file-append psmisc "/bin")
+                                  #$(file-append vulkan-tools "/bin")
+                                  #$(file-append xrandr "/bin")
+                                  #$(file-append xterm "/bin")
+                                  "$PATH"))
+                         `("PYTHONPATH" =
+                           (,(getenv "GUIX_PYTHONPATH")))
+                         `("XDG_DATA_DIRS" =
+                           ,(list #$(file-append hicolor-icon-theme-lutris
+                                                 "/share")
+                                  #$(file-append hicolor-icon-theme "/share")
+                                  #$(file-append shared-mime-info "/share")
+                                  "$XDG_DATA_DIRS"))))))
+      #:install-plan ''(("bin" "bin")
+                        ("lutris" "lutris")
+                        ("share" "share"))))
+    (home-page "https://github.com/lutris/lutris")
+    (synopsis "Game library manager")
+    (description
+     "Lutris is a game manager that can be used as a frontend for many sources of games")
+    (license license:gpl3)))
 
 (define lutris-client-libs-64 ; For TLS support, to login into itch.io, gog, etc.
   `(("nss-certs" ,nss-certs)
